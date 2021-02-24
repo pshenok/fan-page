5f4788d2cd888518c3d42584f512f23e6bcb90dd
Kostyantyn Pshenychnyy <pshenok.dev@gmail.com>
2021-02-24 22:03:29 +0200

    chore: Login / move to create page

---
+++ index.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const puppeteer = require("puppeteer");
+
+const sleep = async (ms) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res();
+    }, ms);
+  });
+};
+
+const LOGIN = process.env.FB_LOGIN;
+const PASSWORD = process.env.FB_PASS;
+const PAGE_NAME = process.env.FB_PAGE_NAME;
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  const page = await browser.newPage();
+
+  const login = async (login, pass) => {
+    await page.goto("https://www.facebook.com/login", {
+      waitUntil: "networkidle2",
+    });
+
+    await page.waitForSelector("#email");
+
+    await page.type("#email", login);
+    await page.type("#pass", pass);
+
+    await page.click("#loginbutton");
+
+    console.log("login done");
+    await page.waitForNavigation();
+  };
+
+  const createFanPage = async (pageName) => {
+    await page.goto("https://www.facebook.com/pages/creation/", {
+      waitUntil: "networkidle2",
+    });
+
+		await page.type("#jsc_c_f", pageName);
+		await page.type("#jsc_c_l", "description");
+
+    await page.screenshot({
+      path: "./facebook.png",
+    });
+  };
+  await login(LOGIN, PASSWORD);
+  await createFanPage(PAGE_NAME);
+})();
